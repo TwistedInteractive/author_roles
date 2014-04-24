9a2bf5a080cb3874c36510d41a594a3a02d93259
elicelso <elicelso@users.noreply.github.com>
2014-04-24 17:30:48 -0300

    Create a brazilian portuguese translation
    Create a brazilian portuguese translation

---
+++ lang/lang.pt-br.php
@@ -0,0 +1,98 @@
+<?php
+
+	$about = array(
+		'name' => 'elicelso',
+		'author' => array(
+			'name' => 'Eli Celso Rios Afonso',
+			'email' => 'elicelso@gmail.com',
+		),
+		'release-date' => '2014-04-24'
+	);
+
+	/**
+	 * Author Roles - Portugues Brasileiro
+	 */
+	$dictionary = array(
+
+		'<strong>Please note:</strong> According to the role you selected, some sections may not be visible to this author. If the <strong>Default Area</strong> of this author is set to one of these hidden sections, it will result in an authorization error message when he or she logs in. Be aware to set the Default Area to an area the author is allowed to see.' => 
+		'<strong>Aviso:</strong> De acordo com a regra selecionada, algumas seções podem não serem visíveis para esse autor. Se a <strong>Área Padrão</strong> desse autor estiver configurada para uma dessas seções ocultas, será apresentada uma mensagem de erro de autorização quando o autor logar ao sistema. Verifique se o autor tem acesso a Área Padrão no perfil dele.',
+
+		'Are you sure you want to delete the selected roles?' => 
+		'Quer mesmo apagar as regras selecionadas?',
+
+		'Author Role' => 
+		'Regra do Autor',
+
+		'Author Roles' => 
+		'Regras do Autor',
+
+		'Author can view/edit own entries only' => 
+		'Autor pode ver/editar somente seus próprios artigos',
+
+		'Authors with this role' => 
+		'Autores com essa regra',
+
+		'Create' => 
+		'Criar',
+
+		'Create Role' => 
+		'Criar Regra',
+
+		'Create a new role' => 
+		'Criar uma nova regra',
+
+		'Custom links' => 
+		'Endereços Personalizáveis',
+
+		'Enter the links of custom menu elements to exclude these links from the main navigation. You can use this to exclude extensions-driven menu-items like Dashboard or Search Index for example. Give one link per line, and enter the URL after the <code>/symphony/</code>-part. For example: <code>/extension/dashboard/index/</code>.' => 
+		'Entre com os links de menus dos elementos customizáveis para excluir esses elementos da navegação principal. Você pode usar isto para excluir itens de menu dirigidas por extensões como o Dashboard ou o Index de Pesquisa. Entre com um link por linha e entre com a URL depois da parte <code>/symphony</code>. Ex: <code>/extension/dashboard/index/</code>.',
+
+		'Filter type' => 
+		'Tipo de filtro',
+
+		'Hide' => 
+		'Ocultar',
+
+		'Hide custom menu elements' => 
+		'Esconder elementos de menu customizáveis',
+
+		'No role assigned' => 
+		'Sem regra associada',
+
+		'None Found.' => 
+		'Nada encontrado.',
+
+		'Role Name' => 
+		'Nome da Regra',
+
+		'Role not saved: Please enter a name.' => 
+		'Regra não salva: Favor entrar com o nome.',
+
+		'Role successfully created/updated.' => 
+		'Regra criada/atualizada com sucesso.',
+
+		'Roles' => 
+		'Regras',
+
+		'This author belongs to' => 
+		'Este autor pertence a',
+
+		'Use filter' => 
+		'Usar filtros',
+
+		'Visible' => 
+		'Visível',
+
+		'You cannot hide required fields' => 
+		'Você não pode ocultar campos requeridos',
+
+		'required' => 
+		'requerido',
+
+		'ID\'s' => 
+		'ID\'s',
+
+		'Enter a list of comma-seperated ID\'s. Define ranges with a hyphen.<br />Example: <code>2,3,6-11,13</code>' => 
+		'Entre com uma lista dos ID\'s separada por virgulas. Limite faixas com hífen.<br />Ex: <code>2,3,6-11,13</code>',
+
+	);
